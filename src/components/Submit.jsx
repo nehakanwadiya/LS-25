48b8b6da4c715d0840fa08a059b705f23f3c1e9d
Thunder25Beast <147324100+Thunder25Beast@users.noreply.github.com>
2025-07-02 13:47:31 +0530

    update

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -7,7 +7,7 @@ function Submit() {
     <div className="submit-body">
       <h2 classame="title-submit">How to Submit Your Project to WebCraft</h2>
       <ul id="main">
-        <li><b>1. Fork the Repository:</b> <br/> Visit the GitHub repository and click on Fork. This
+        <li><b>1. Fork the Repository:</b> <br/> Visit the <a href="https://github.com/ITC-Web-Team/LS-25" target="_blank" rel="noopener noreferrer">GitHub repository</a> and click on Fork. This
 will create a copy under your GitHub account. 
 </li> <br/>
         <li><b>2. Clone Your Fork:</b> <br/>
@@ -31,18 +31,20 @@ will create a copy under your GitHub account.
 <li><b>5. Metadata Format:</b><br/>In index.jsx, include: <br/>
 <img src='./index-ex.png'></img>
 </li><br/>
-  <li>📝 <code>meta</code> is used to generate the card for your app on the homepage.</li><br/>
+  <li><code>meta</code> is used to generate the card for your app on the homepage.</li><br/>
   <li>Thumbnail is optional but recommended</li><br/>
   <ul><li>Place thumbnail image in: <code>public/assets/tumbnails/</code></li><br/>
   <li>Use the correct public path in your: <code>meta.thumbnail</code></li></ul><br/>
-<li><b>6. Add and Commit Your Changes:</b><br/><code>git add . <br/>
+  <li><b>6. Test Locally Before Submitting</b></li>
+<code>npm run dev<br/> </code> <br/>
+<li><b>7. Add and Commit Your Changes:</b><br/><code>git add . <br/>
 git commit -m "Added Bob's analog clock project"</code></li><br/>
-<li><b>7. Push Your Branch and Create a Pull Request:</b><br/>
+<li><b>8. Push Your Branch and Create a Pull Request:</b><br/>
 <code>git push origin main</code><br/>
 Then go to GitHub and open a Pull Request from your forked repo's main branch to the original main branch.
 </li><br/>
 
-<li><b>8. Common mistakes to avoid</b><br/>
+<li><b>9. Common mistakes to avoid</b><br/>
 <ul>
 <li>Not using a unique folder name.</li>
 <li>Spaces in folder names(we highly recommend kebab-case).
@@ -53,9 +55,8 @@ This is recommended because the name of your folder will appear on URL, like, <c
 <li>Thumbnail path is wrong(pt. 5)</li>
 </ul><br/>
 
-<li><b>9. Test Locally Before Submitting</b></li>
-<code>npm run dev</code>
-</li><br/>
+
+</li>
       </ul>
     </div>
   )
