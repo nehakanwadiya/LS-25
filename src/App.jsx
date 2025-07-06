c55f67f7529ca1e2abdace7c7ea484a2fdc794c9
nehakanwadiya <kanwadiyaneha4@gmail.com>
2025-07-07 02:36:28 +0530

    Added metadata.json for LS-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import CardList from './components/CardList'
-import Sidebar from './components/Sidebar'
-import Mentor from './components/Mentors'
-import Submit from './components/Submit'
-import MenteeWrapper from './components/MenteeWrapper'
-import ScrollToTopButton from './components/ScrollToTop'
-import useScrollToTop from './hooks/useScrollToTop'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CardList from './components/CardList';
+import Sidebar from './components/Sidebar';
+import Mentor from './components/Mentors';
+import Submit from './components/Submit';
+import MenteeWrapper from './components/MenteeWrapper';
+import ScrollToTopButton from './components/ScrollToTop';
+import useScrollToTop from './hooks/useScrollToTop';
 
 function AppContent() {
   useScrollToTop();
-  
+
   return (
     <>
       <Sidebar />
       <main className="main-content">
+        {/* 🌟 Project Showcase Banner */}
+        <div
+          style={{
+            textAlign: 'center',
+            marginTop: '20px',
+            marginBottom: '10px',
+            fontFamily: 'Poppins, sans-serif',
+          }}
+        >
+          <h1 style={{ fontSize: '32px', color: '#ff6b81', marginBottom: '5px' }}>
+            "WebCraft Project"
+          </h1>
+          <p style={{ fontSize: '16px', color: '#444' }}>
+            🚀 Presenting <strong>SplitNow+</strong> – A stylish, one-tap expense splitter for your gang trips, chai breaks & more!
+          </p>
+        </div>
+
+        {/* All Routes Below */}
         <Routes>
           <Route path="/" element={<CardList />} />
           <Route path="/details/:name" element={<MenteeWrapper />} />
@@ -23,7 +41,7 @@ function AppContent() {
       </main>
       <ScrollToTopButton />
     </>
-  )
+  );
 }
 
 function App() {
@@ -31,7 +49,7 @@ function App() {
     <Router>
       <AppContent />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
